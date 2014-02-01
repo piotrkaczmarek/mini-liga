6b2f32cd2815ed994ffc2e1fb17a891821a1f9b2
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2014-02-01 19:37:17 +0100

    Adds flash messages translations and sets default locale to pl

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,9 +4,9 @@ class PasswordResetsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user
       user.send_password_reset
-      flash[:notice] = "Email with password reset instructions sent"
+      flash[:notice] = (t :password_reset_sent)
     else
-      flash[:error] = "No such email registered"
+      flash[:error] = (t :no_such_email)
     end
     redirect_to root_url 
   end
@@ -18,10 +18,10 @@ class PasswordResetsController < ApplicationController
   def update
     if @user = User.find_by_password_reset_token(params[:id])
       if @user.password_reset_sent_at < 2.hours.ago
-        flash[:error] = "Password reset has expired."
+        flash[:error] = (t :password_expired)
         redirect_to new_password_reset_path
       elsif @user.update_attributes(user_params["#{@user.class.name.downcase}"])
-        flash[:notice] = "Password has been reseted!"
+        flash[:notice] = (t :password_reseted)
         redirect_to root_url
       else
         render :edit
